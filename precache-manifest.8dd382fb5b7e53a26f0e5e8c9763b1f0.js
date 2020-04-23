4c3541ec52bc4adc70e1780dcdfef5ed3f2998cd
Nick_Khoroshkov <nickolaykhoroshkov@gmail.com>
2020-04-23 11:52:21 +0300

    Updates

---
+++ precache-manifest.8dd382fb5b7e53a26f0e5e8c9763b1f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1324f2e611a350a13ae597f4a4adcbca",
+    "url": "/react-typescript/index.html"
+  },
+  {
+    "revision": "50d74664a3054efb231f",
+    "url": "/react-typescript/static/css/main.2e0d2a24.chunk.css"
+  },
+  {
+    "revision": "3171afaa8c0b08b22292",
+    "url": "/react-typescript/static/js/2.7a6805f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-typescript/static/js/2.7a6805f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50d74664a3054efb231f",
+    "url": "/react-typescript/static/js/main.c9e8d873.chunk.js"
+  },
+  {
+    "revision": "3466d42cc36e1a480b4e",
+    "url": "/react-typescript/static/js/runtime-main.05d7fab9.js"
+  }
+]);
